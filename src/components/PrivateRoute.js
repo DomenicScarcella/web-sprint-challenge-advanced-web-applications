5d0d79073faac661940e340da8427d6213442115
Domenic Scarcella <ls41513@yahoo.com>
2021-03-26 03:57:05 -0400

    PrivateRoute copied from Tue. project

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,4 +1,22 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (window.localStorage.getItem('token')) {
+          return <Component />
+        } else {
+          return <Redirect to='/login' />;
+        }
+      }}
+    />
+  );
+}
+
+export default PrivateRoute;
 
 
 
